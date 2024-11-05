8a65bdcf01e917e7df08db908b6045f479e51ae0
archengdev <106987393+archengdev@users.noreply.github.com>
2024-11-05 23:23:20 +0000

    finished project

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def backdoor
+    render({ :template => "item_templates/backdoor"})
+  end
+  def insert
+    itm = Item.new
+    itm.link_url = params.fetch("query_link_url")
+    itm.link_description = params.fetch("query_link_description")
+    itm.thumbnail_url = params.fetch("query_thumbnail_url")
+    itm.save
+
+    redirect_to("/")
+
+  end
 end
